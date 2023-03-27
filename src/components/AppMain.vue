b6d275f1baae3f86b2864e53bba3b0a026875f15
giovannitamburini <giovatambu@gmail.com>
2023-03-27 16:33:44 +0200

    completed exercise

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,108 @@
-<script></script>
+<script>
+
+import AppSeriesCard from "./AppSeriesCard.vue";
+
+export default {
+    data() {
+        return {
+            cards: [
+                {
+                    thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    price: "$19.99",
+                    series: "Action Comics",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    price: "$3.99",
+                    series: "American Vampire 1976",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    price: "$16.99",
+                    series: "Aquaman",
+                    type: "graphic novel"
+                },
+                {
+                    thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    price: "$2.99",
+                    series: "Batgirl",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    price: "$3.99",
+                    series: "Batman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    price: "$2.99",
+                    series: "Batman Beyond",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    price: "$3.99",
+                    series: "Batman/Superman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    price: "$4.99",
+                    series: "Batman/Superman Annual",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    price: "$5.99",
+                    series: "Batman: The Joker War Zone",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    price: "$6.99",
+                    series: "Batman: Three Jokers",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    price: "$4.99",
+                    series: "Batman: White Knight Presents: Harley Quinn",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    price: "$16.99",
+                    series: "Catwoman",
+                    type: "graphic novel"
+                },
+            ],
+        };
+    },
+
+    components: {
+        AppSeriesCard,
+    }
+}
+</script>
 
 <template>
     <div id="main-container">
         <div id="main-background">
             <img src="/img/jumbotron.jpg" alt="">
+        </div>
+
+        <div id="main-series">
+            <div id="current">CURRENT SERIES</div>
+
+            <div id="series-container">
+                <AppSeriesCard v-for="card in cards" :imgCard="card.thumb" :titleCard="card.series"></AppSeriesCard>
+            </div>
+
+
+            <button id="load">LOAD MORE</button>
         </div>
     </div>
 </template>
@@ -12,10 +111,50 @@
 #main-background {
     height: 280px;
     overflow-y: hidden;
-    position: relative;
 
     img {
+        display: block;
         width: 100%;
+    }
+}
+
+#main-series {
+    max-width: 1000px;
+    margin: 0 auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    #current {
+        display: inline-block;
+        padding: 7px 20px;
+        background-color: #0282f9;
+        font-size: 1.6em;
+        font-weight: 600;
+        color: white;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transform: translateY(-50%);
+    }
+
+    #series-container {
+        width: 100%;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-around;
+        padding-top: 35px;
+    }
+
+    #load {
+        display: inline-block;
+        padding: 8px 40px;
+        background-color: #0282f9;
+        border: none;
+        color: white;
+        font-weight: 600;
+        margin: 15px 0;
     }
 }
 </style>
---
+++ src/components/AppSeriesCard.vue
@@ -0,0 +1,50 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        imgCard: String,
+        titleCard: String,
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-img">
+            <img :src="imgCard" alt="">
+        </div>
+        <div class="series">{{ titleCard }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: 165px;
+    padding: 10px;
+    color: white;
+    text-transform: uppercase;
+
+    .card-img {
+        width: 100%;
+        height: 165px - (10px * 2);
+        overflow-y: hidden;
+
+
+        img {
+            width: 100%;
+        }
+    }
+
+    .series {
+        padding: 1em 0;
+        word-wrap: break-word;
+    }
+}
+</style>
